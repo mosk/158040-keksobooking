f2df85ad0e5b4e2270e0ce3c7d84050dbde2a79c
mosk <miiixsc@gmail.com>
2017-12-18 13:29:45 +0300

    внёс правки

--- js/map.js
+++ js/map.js
@@ -3,7 +3,7 @@
 var NUMBER_OF_ADVERTS = 8;
 var map = document.querySelector('.map');
 var mapPins = document.querySelector('.map__pins');
-var ADVERTS = getAdverts(NUMBER_OF_ADVERTS);
+var adverts = getAdverts(NUMBER_OF_ADVERTS);
 
 function getRandomNumber(min, max) {
   return Math.random() * (max - min) + min;
@@ -19,7 +19,7 @@ function getNewArray(array) {
   var newArray = [];
 
   for (var i = 0; i < array.length; i++) {
-    if (Math.round(Math.random(0, 1)) === 1) {
+    if (Math.round(Math.random()) === 1) {
       newArray.push(array[i]);
     }
   }
@@ -27,7 +27,7 @@ function getNewArray(array) {
   return newArray;
 }
 
-// получаем случайное объявление в видн объекта
+// получаем случайное объявление в виде объекта
 function getRandomAdvert() {
   var TITLES = ['Большая уютная квартира', 'Маленькая неуютная квартира', 'Огромный прекрасный дворец', 'Маленький ужасный дворец', 'Красивый гостевой домик', 'Некрасивый негостеприимный домик', 'Уютное бунгало далеко от моря', 'Неуютное бунгало по колено в воде', 'Проклятый старый дом'];
   var TYPES = ['flat', 'house', 'bungalo'];
@@ -45,9 +45,17 @@ function getRandomAdvert() {
   var LOCATION_Y_MIN = 100;
   var LOCATION_Y_MAX = 500;
 
+  function getNewSrc(initialSrc, number, srcEnding) {
+    if (number > 9) {
+      return initialSrc + number + srcEnding;
+    }
+
+    return initialSrc + '0' + number + srcEnding;
+  }
+
   return {
     author: {
-      avatar: 'img/avatars/user' + 0 + Math.round(getRandomNumber(1, NUMBER_OF_ADVERTS)) + '.png'
+      avatar: getNewSrc('img/avatars/user', Math.round(getRandomNumber(1, NUMBER_OF_ADVERTS)), '.png')
     },
     offer: {
       title: getRandomElement(TITLES),
@@ -71,23 +79,23 @@ function getRandomAdvert() {
 
 // массив с объявлениями
 function getAdverts(amount) {
-  var adverts = [];
+  var advertsArray = [];
   for (var i = 0; i < amount; i++) {
-    adverts.push(getRandomAdvert());
+    var newAdvert = getRandomAdvert();
+    advertsArray.push(newAdvert);
   }
 
-  return adverts;
+  return advertsArray;
 }
 
 // создаю метку с объявлением
 function renderAdvertPin(advert) {
   var template = document.querySelector('template');
   var advertPin = template.content.querySelector('.map__pin').cloneNode(true);
-  var advertPinWidth = 48;
-  var advertPinHeight = 60;
+  var advertPinOffsetY = 38; // расстояние от центра пина до острия маркера
 
-  advertPin.style.left = advert.location.x + advertPinWidth / 2 + 'px';
-  advertPin.style.top = advert.location.y - advertPinHeight + 'px';
+  advertPin.style.left = advert.location.x + 'px'; // У 'map__pin' свойство 'transform: translate(-50%, -50%)', следовательно старый вариант не нужен? старый вариант *** advertPin.style.left = advert.location.x + advertPinWidth / 2 + 'px'; ***
+  advertPin.style.top = advert.location.y - advertPinOffsetY + 'px';
   advertPin.querySelector('img').setAttribute('src', advert.author.avatar);
   advertPin.querySelector('img').setAttribute('draggable', 'false');
 
@@ -96,26 +104,28 @@ function renderAdvertPin(advert) {
 
 function getOfferType(type) {
   var TYPES = {
-    items: ['flat', 'house', 'bungalo'],
-    names: ['Квартира', 'Бунгало', 'Дом']
+    flat: 'Квартира',
+    house: 'Дом',
+    bungalo: 'Бунгало'
   };
 
-  for (var i = 0; i < TYPES.items.length; i++) {
-    if (type === TYPES.items[i]) {
-      var typeName = TYPES.names[i];
+  for (var i = 0; i < Object.keys(TYPES).length; i++) {
+    if (type === Object.keys(TYPES)[i]) {
+      var typeKey = Object.keys(TYPES)[i];
+      var typeName = TYPES[typeKey];
     }
   }
 
   return typeName;
 }
 
-// проверка на окончание (в задание такого нет - сделал для себя)
+// проверка на окончание (в задании такого нет - сделал для себя)
 function getRightEnding(number) {
   var stringNumber = String(number);
   var lastNumber = +stringNumber.charAt(stringNumber.length - 1);
   var preLastNumber = +stringNumber.charAt(stringNumber.length - 2);
 
-  if ([1].indexOf(preLastNumber) > -1 || [5, 6, 7, 8, 9, 0].indexOf(lastNumber) > -1) {
+  if (preLastNumber === 1 || [5, 6, 7, 8, 9, 0].indexOf(lastNumber) > -1) {
     return '';
   }
 
@@ -129,28 +139,26 @@ function getRightEnding(number) {
 function renderAdvertArticle(advert) {
   var template = document.querySelector('template');
   var advertArticle = template.content.querySelector('.map__card').cloneNode(true);
-  var advertTitle = advertArticle.querySelector('h3');
-  var advertAddress = advertArticle.querySelector('p');
-  var advertPrice = advertArticle.querySelector('.popup__price');
-  var advertType = advertArticle.querySelector('h4');
-  var advertRooms = advertArticle.querySelector(':nth-child(7)');
-  var advertCheckin = advertArticle.querySelector(':nth-child(8)');
-  var advertDescription = advertArticle.querySelector(':nth-child(10)');
+  var advertTitle = advertArticle.querySelector('.card__title');
+  var advertAddress = advertArticle.querySelector('.card__address');
+  var advertPrice = advertArticle.querySelector('.card__price');
+  var advertType = advertArticle.querySelector('.card__type');
+  var advertRooms = advertArticle.querySelector('.card__rooms');
+  var advertCheckin = advertArticle.querySelector('.card__checkin');
+  var advertDescription = advertArticle.querySelector('.card__description');
   var advertAvatar = advertArticle.querySelector('.popup__avatar');
   var advertFeatures = advertArticle.querySelector('.popup__features');
+  var fragment = document.createDocumentFragment();
 
-  advertArticle.classList.add('card');
-  advertTitle.classList.add('card__title');
   advertTitle.textContent = advert.offer.title;
-  advertAddress.classList.add('card__address');
   advertAddress.textContent = advert.offer.address;
 
   advertPrice.textContent = advert.offer.price + ' &#x20bd;/ночь';
   advertType.textContent = getOfferType(advert.offer.type);
+
   advertRooms.textContent = advert.offer.rooms + ' комнат' + getRightEnding(advert.offer.rooms) + ' для ' + advert.offer.guests + ' гостей';
   advertCheckin.textContent = 'Заезд после ' + advert.offer.checkin + ', выезд до ' + advert.offer.checkout;
 
-  advertDescription.classList.add('card__description');
   advertDescription.textContent = advert.offer.description;
   advertAvatar.setAttribute('src', advert.author.avatar);
 
@@ -167,27 +175,21 @@ function renderAdvertArticle(advert) {
     newAdvertFeatures.appendChild(advertFeature);
   }
 
-  return advertArticle;
+  fragment.appendChild(advertArticle);
+
+  return fragment;
 }
 
-function renderAdvertPins(adverts) {
+function renderAdvertPins(advertsArray) {
   var fragment = document.createDocumentFragment();
 
-  for (var i = 0; i < adverts.length; i++) {
-    fragment.appendChild(renderAdvertPin(adverts[i]));
+  for (var i = 0; i < advertsArray.length; i++) {
+    fragment.appendChild(renderAdvertPin(advertsArray[i]));
   }
 
   return fragment;
 }
 
-function renderAdvertArticles(adverts) {
-  var fragment = document.createDocumentFragment();
-
-  fragment.appendChild(renderAdvertArticle(adverts[0]));
-
-  return fragment;
-}
-
 map.classList.remove('map--faded');
-mapPins.appendChild(renderAdvertPins(ADVERTS));
-map.insertBefore(renderAdvertArticles(ADVERTS), map.querySelector('.map__filters-container'));
+mapPins.appendChild(renderAdvertPins(adverts));
+map.insertBefore(renderAdvertArticle(adverts[0]), map.querySelector('.map__filters-container'));
